a0d634aaf7b91bbdfcb757cf12c41e638af7b47f
Makihara <clmakihara@gmail.com>
2021-06-14 23:12:33 -0300

    Incluido Entidades e Enum.

---
+++ src/main/java/one/digitalinnovation/personapi/enuns/PhoneType.java
@@ -0,0 +1,15 @@
+package one.digitalinnovation.personapi.enuns;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum PhoneType {
+
+    HOME,
+    MOBILE,
+    COMMERCIAL;
+
+   // private final String description;
+}
